200f5837fb2dfa881e6cecf28bd4fb650c1c6831
Shnaider Danila <shnd3r@yandex.ru>
2023-06-27 13:31:32 +0700

    feat: Портировать разметку HTML в App.js при помощи JSX

--- src/App.js
+++ src/App.js
@@ -3,23 +3,108 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="wrapper">
+        <header className="header">
+          <img className="header__logo" src="<%=require('./images/logo.svg')%>" alt="Логотип проекта - Место Россия" />
+        </header>
+        <main className="main">
+          <section className="profile">
+            <button className="profile__avatar-button" type="button">
+              <img className="profile__avatar-image" alt="Аватарка пользователя" />
+              <div className="profile__avatar-overlay"></div>
+            </button>
+            <div className="profile__info">
+              <h1 className="profile__title"></h1>
+              <button className="profile__button-edit" type="button"></button>
+              <p className="profile__subtitle"></p>
+            </div>
+            <button className="profile__button-add" type="button"></button>
+          </section>
+          <section className="gallery">
+            <ul className="gallery__list"></ul>
+          </section>
+        </main>
+        <footer className="footer">
+          <p className="footer__copyright">© 2023 Mesto Russia</p>
+        </footer>
+        <div className="popup popup-profile">
+          <div className="popup__container">
+            <button className="popup__button-close" type="button"></button>
+            <h3 className="popup__title">Редактировать профиль</h3>
+            <form className="popup__form" name="edit-profile" novalidate>
+              <label className="popup__form-field">
+                <input id="username" className="popup__input" type="text" name="name" placeholder="Имя" required minlength="2" maxlength="40" />
+                <span id="error-username" className="popup__input-error"></span>
+              </label>
+              <label className="popup__form-field">
+                <input id="about" className="popup__input" type="text" name="about" placeholder="Профессия" required minlength="2" maxlength="200" />
+                <span id="error-about" className="popup__input-error"></span>
+              </label>
+              <button className="popup__button-save" type="submit">Сохранить</button>
+            </form>
+          </div>
+        </div>
+        <div className="popup popup-add">
+          <div className="popup__container">
+            <button className="popup__button-close" type="button"></button>
+            <form className="popup__form" name="add-card" novalidate>
+              <h3 className="popup__title">Новое место</h3>
+              <label className="popup__form-field">
+                <input id="place" className="popup__input" type="text" name="place" placeholder="Название" required minlength="2" maxlength="30" />
+                <span id="error-place" className="popup__input-error"></span>
+              </label>
+              <label className="popup__form-field">
+                <input id="link" className="popup__input" type="url" name="link" placeholder="Ссылка на картинку" required />
+                <span id="error-link" className="popup__input-error"></span>
+              </label>
+              <button className="popup__button-save" type="submit">Создать</button>
+            </form>
+          </div>
+        </div>
+        <div className="popup popup-picture">
+          <figure className="popup__figure">
+            <button className="popup__button-close" type="button"></button>
+            <img className="popup__big-image" />
+            <figcaption className="popup__figcaption"></figcaption>
+          </figure>
+        </div>
+        <div className="popup popup-alert">
+          <div className="popup__container">
+            <button className="popup__button-close" type="button"></button>
+            <p className="popup__alert-text popup__title">Вы уверены?</p>
+            <button className="popup__button-save" type="button">Да</button>
+          </div>
+        </div>
+        <div className="popup popup-avatar">
+          <div className="popup__container">
+            <button className="popup__button-close" type="button"></button>
+            <h3 className="popup__title">Обновить аватар</h3>
+            <form className="popup__form" name="avatar-update" novalidate>
+              <label className="popup__form-field">
+                <input id="avatar" className="popup__input" type="url" name="avatar" placeholder="Ссылка на картинку" required />
+                <span id="error-avatar" className="popup__input-error"></span>
+              </label>
+            <button className="popup__button-save" type="submit">Сохранить</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <template id="card-template">
+        <li className="card">
+          <img className="card__image" />
+          <button className="card__delete-button" type="button"></button>
+          <div className="card__description">
+            <h2 className="card__title"></h2>
+            <div className="card__like">
+              <button className="card__like-button" type="button"></button>
+              <p className="card__like-count"></p>
+            </div>
+          </div>
+        </li>
+      </template>
     </div>
-  );
-}
+  )
+};
 
 export default App;
