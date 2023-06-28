d3fbae95ef58ebf2b320fd94063a0c0b75ba29e5
Shnaider Danila <shnd3r@yandex.ru>
2023-06-28 15:43:36 +0700

    feat: Добавить компонент Card

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+function Card(props) {
+  return (
+    <li key={props.card._id} className="card">
+      <img className="card__image" src={props.card.link} alt={props.card.name} />
+      <button className="card__delete-button" type="button"></button>
+      <div className="card__description">
+        <h2 className="card__title">{props.card.name}</h2>
+        <div className="card__like">
+          <button className="card__like-button" type="button"></button>
+          <p className="card__like-count">{props.card.likes.length}</p>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default Card
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { api } from "../utils/Api.js";
+import Card from "./Card.js";
 
 function Main(props) {
   const [userName, setUserName] = React.useState('Жак-Ив Кусто');
@@ -12,7 +13,7 @@ function Main(props) {
       setUserName(userInfo.name);
       setUserDescription(userInfo.about);
       setUserAvatar(userInfo.avatar);
-      setCards([...cards, ...initialCards])
+      setCards([...initialCards])
     })
   }, [])
 
@@ -32,20 +33,9 @@ function Main(props) {
       </section>
       <section className="gallery">
         <ul className="gallery__list">
-          {cards.map(card => {
-            return (
-            <li key={card._id} className="card">
-              <img className="card__image" src={card.link} />
-              <button className="card__delete-button" type="button"></button>
-              <div className="card__description">
-                <h2 className="card__title">{card.name}</h2>
-                <div className="card__like">
-                  <button className="card__like-button" type="button"></button>
-                  <p className="card__like-count">{card.likes.length}</p>
-                </div>
-              </div>
-            </li>
-          )})}  
+          {cards.map((card) => (
+             <Card key={card._id} card={card}/>
+          ))}
         </ul>
       </section>
     </main>
