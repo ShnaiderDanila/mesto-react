165a587aae7b6ee3b1a88c4d8dd8a6fa7c2af28e
Shnaider Danila <shnd3r@yandex.ru>
2023-06-28 18:19:48 +0700

    feat: Добавить описание открытым картинкам в компоненте ImagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,8 +3,8 @@ function ImagePopup(props) {
     <div className={`popup popup-picture ${props.card ? 'popup_is-opened' : ''}`}>
       <figure className="popup__figure">
         <button onClick={props.onClose} className="popup__button-close" type="button"></button>
-        <img className="popup__big-image" src={props.card.link}/>
-        <figcaption className="popup__figcaption"></figcaption>
+        <img className="popup__big-image" src={props.card.link} alt={props.card.name}/>
+        <figcaption className="popup__figcaption">{props.card.name}</figcaption>
       </figure>
     </div>
   )
