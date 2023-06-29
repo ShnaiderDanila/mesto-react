55037911128a4926454656caaea44553702f7bcd
Shnaider Danila <shnd3r@yandex.ru>
2023-06-29 17:24:12 +0700

    refactor: Изменить тип данных стейт-переменных в App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-  const [selectedCard, setSelectedCard] = React.useState(false)
+  const [selectedCard, setSelectedCard] = React.useState({name: '', link: ''});
 
   function handleEditProfileClick() {
     setIsEditProfilePopupOpen(true);
@@ -31,7 +31,7 @@ function App() {
     isEditProfilePopupOpen && setIsEditProfilePopupOpen(false);
     isAddPlacePopupOpen && setIsAddPlacePopupOpen(false);
     isEditAvatarPopupOpen && setIsEditAvatarPopupOpen(false);
-    selectedCard && setSelectedCard(false);
+    selectedCard && setSelectedCard({name: '', link: ''});
   }
 
   return (
