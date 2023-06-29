0f6e876b4ded7f5e664e32e1cd6275f87ab52471
Shnaider Danila <shnd3r@yandex.ru>
2023-06-29 18:09:39 +0700

    refactor: Деструктурирован объект props и убран тег novalidate

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,12 +1,12 @@
-function PopupWithForm(props) {
+function PopupWithForm({isOpen, onClose, name, title, buttonText, children}) {
   return (
-    <div className={`popup popup-${props.name} ${props.isOpen ? 'popup_is-opened' : ''}`}>
+    <div className={`popup popup-${name} ${isOpen ? 'popup_is-opened' : ''}`}>
       <div className="popup__container">
-        <h3 className="popup__title">{props.title}</h3>
-        <button onClick={props.onClose} className="popup__button-close" type="button"></button>
-        <form className="popup__form" name={props.name} noValidate>
-          {props.children}
-          <button className="popup__button-save" type="submit">{props.buttonText}</button>
+        <h3 className="popup__title">{title}</h3>
+        <button onClick={onClose} className="popup__button-close" type="button"/>
+        <form className="popup__form" name={name}>
+          {children}
+          <button className="popup__button-save" type="submit">{buttonText}</button>
         </form>
       </div>
     </div>
