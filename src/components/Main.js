4c64abdbf0cc591fc65a72cd72fce639bbb9cb60
Shnaider Danila <shnd3r@yandex.ru>
2023-07-12 14:09:32 +0700

    feat: Подписать компонент Main.js на контекст CurrentUserContext

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { api } from "../utils/Api.js";
 import Card from "./Card.js";
+import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 
 function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
-  const [userName, setUserName] = React.useState('Жак-Ив Кусто');
-  const [userDescription, setUserDescription] = React.useState('Исследователь океана');
-  const [userAvatar, setUserAvatar] = React.useState('');
+
   const [cards, setCards] = React.useState([]);
+  const {name, about, avatar} = React.useContext(CurrentUserContext)
   
   React.useEffect(() => {
-    api.getAppInfo()
-    .then(([initialCards, userInfo]) => {
-      setUserName(userInfo.name);
-      setUserDescription(userInfo.about);
-      setUserAvatar(userInfo.avatar);
+    api.getInitialCards()
+    .then((initialCards) => {
       setCards([...initialCards])
     })
     .catch((err) => {
@@ -25,13 +22,13 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
     <main className="main">
       <section className="profile">
         <button onClick={onEditAvatar} className="profile__avatar-button" type="button">
-          <img className="profile__avatar-image" alt="Аватарка пользователя" src={userAvatar} />
+          <img className="profile__avatar-image" alt="Аватарка пользователя" src={avatar} />
           <div className="profile__avatar-overlay"></div>
         </button>
         <div className="profile__info">
-          <h1 className="profile__title">{userName}</h1>
+          <h1 className="profile__title">{name}</h1>
           <button onClick={onEditProfile} className="profile__button-edit" type="button"></button>
-          <p className="profile__subtitle">{userDescription}</p>
+          <p className="profile__subtitle">{about}</p>
         </div>
         <button onClick={onAddPlace} className="profile__button-add" type="button"></button>
       </section>
