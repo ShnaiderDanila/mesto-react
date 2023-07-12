63d671dbb42817d82505b2c3ad4ca4727476760f
Shnaider Danila <shnd3r@yandex.ru>
2023-07-12 14:15:30 +0700

    fix: Исправить имя переменной контекста

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,9 @@ import Card from "./Card.js";
 import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 
 function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
-
+  
   const [cards, setCards] = React.useState([]);
-  const {name, about, avatar} = React.useContext(CurrentUserContext)
+  const currentUser = React.useContext(CurrentUserContext)
   
   React.useEffect(() => {
     api.getInitialCards()
@@ -22,13 +22,13 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
     <main className="main">
       <section className="profile">
         <button onClick={onEditAvatar} className="profile__avatar-button" type="button">
-          <img className="profile__avatar-image" alt="Аватарка пользователя" src={avatar} />
+          <img className="profile__avatar-image" alt="Аватарка пользователя" src={currentUser.avatar} />
           <div className="profile__avatar-overlay"></div>
         </button>
         <div className="profile__info">
-          <h1 className="profile__title">{name}</h1>
+          <h1 className="profile__title">{currentUser.name}</h1>
           <button onClick={onEditProfile} className="profile__button-edit" type="button"></button>
-          <p className="profile__subtitle">{about}</p>
+          <p className="profile__subtitle">{currentUser.about}</p>
         </div>
         <button onClick={onAddPlace} className="profile__button-add" type="button"></button>
       </section>
