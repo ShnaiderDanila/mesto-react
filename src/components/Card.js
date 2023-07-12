c22962e136feb865e588d0020affdf9976c24770
Shnaider Danila <shnd3r@yandex.ru>
2023-07-12 14:08:54 +0700

    feat: Подписать Card.js на контекст CurrentUserContext

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,9 @@
+import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
+
 function Card({card, onCardClick}) {
+
+  const {_id} = React.useContext(CurrentUserContext);
   
   function handleImageClick() {
     onCardClick(card);
