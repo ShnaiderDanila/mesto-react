23af94ac7e1d4af0411a6c8b4d3197a76b2f8731
Shnaider Danila <shnd3r@yandex.ru>
2023-07-13 14:58:53 +0700

    feat: Добавить props - onSubmit

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,10 +1,10 @@
-function PopupWithForm({isOpen, onClose, name, title, buttonText, children}) {
+function PopupWithForm({isOpen, onClose, onSubmit, name, title, buttonText, children}) {
   return (
     <div className={`popup popup-${name} ${isOpen ? 'popup_is-opened' : ''}`}>
       <div className="popup__container">
         <h3 className="popup__title">{title}</h3>
         <button onClick={onClose} className="popup__button-close" type="button"/>
-        <form className="popup__form" name={name}>
+        <form onSubmit={onSubmit} className="popup__form" name={name}>
           {children}
           <button className="popup__button-save" type="submit">{buttonText}</button>
         </form>
